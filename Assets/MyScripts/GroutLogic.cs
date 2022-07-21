520fa4b70f6056c4d3d094319fdb1cfc4ec38f8b
wvagen <36245246+wvagen@users.noreply.github.com>
2022-07-21 13:46:46 +0100

    Version 3.0

---
+++ Assets/MyScripts/GroutLogic.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroutLogic : MonoBehaviour
+{
+    public bool iDidWell = false;
+
+    Transform holePos = null;
+
+    float distance;
+    TrailRenderer myTrail;
+    SpriteRenderer mySprite;
+    Vector2 firstPos;
+    bool weCanTalkNow = false;
+    bool isCalculating = false;
+
+    const float minDistanceToConsider = 0.1f;
+
+    bool isFlip = true;
+    void Start()
+    {
+        myTrail = GetComponent<TrailRenderer>();
+        mySprite = GetComponent<SpriteRenderer>();
+        StartCoroutine(fadeMe());
+        myTrail.enabled = false;
+        firstPos = transform.position;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if ((col.gameObject.tag == "crack" || col.gameObject.tag == "bigCrack") && !Manager.isGameOver)
+        {
+            holePos = col.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (!weCanTalkNow && Vector2.Distance(firstPos,transform.position) > minDistanceToConsider)
+        {
+            if (transform.position.x > firstPos.x)
+            {
+                isFlip = true;
+                mySprite.flipX = true;
+            }
+            else
+            {
+                isFlip = false;
+            }
+            weCanTalkNow = true;
+            myTrail.enabled = true;
+        }
+        if (weCanTalkNow && holePos != null && !isCalculating)
+        {
+            QuickMaths();
+        }
+    }
+    IEnumerator fadeMe()
+    {
+        Color colWhite = Color.white;
+        while (colWhite.a > 0)
+        {
+            colWhite.a -= Time.deltaTime;
+            mySprite.color = colWhite;
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(this.gameObject);
+
+    }
+
+    void QuickMaths()
+    {
+       /* distance = Vector2.Distance(transform.position, holePos.position);
+        if (distance < (holePos.transform.localScale.x / 2) * Mathf.Sqrt(2))
+        {*/
+        isCalculating = true;
+        holePos.GetComponent<Crack>().fadeAnimation();
+        iDidWell = true;
+        holePos.GetComponent<Crack>().endLoop(isFlip);
+        isCalculating = false;
+       // }
+    }
+
+}
---
+++ Assets/MyScripts/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+
+    public GameObject smallGrout;
+    public Manager man;
+
+    GameObject lastGroutCreated;
+    Vector2 mousePos;
+    bool isMouseDown = false;
+    //bool isSmallGroutSpawned = true;
+
+    void Update()
+    {
+        if (Manager.isPaused || Manager.isGameOver) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnDown();
+
+        }
+        if (Input.GetMouseButtonUp(0)) OnUp();
+        if (isMouseDown) followMyMouse();
+    }
+
+    void OnDown()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lastGroutCreated = Instantiate(smallGrout, mousePos, Quaternion.identity);
+            lastGroutCreated.GetComponent<TrailRenderer>().startWidth *= 1.5f;
+            lastGroutCreated.GetComponent<TrailRenderer>().endWidth *= 1.5f;
+        Manager.orderInLayer++;
+        lastGroutCreated.GetComponent<TrailRenderer>().sortingOrder = Manager.orderInLayer;
+        lastGroutCreated.GetComponent<SpriteRenderer>().sortingOrder = Manager.orderInLayer;
+        isMouseDown = true;
+    }
+
+    void OnUp()
+    {
+        if (lastGroutCreated != null)
+        {
+            if (lastGroutCreated.GetComponent<GroutLogic>().iDidWell)
+                man.boostCombot();
+            else
+                man.resetCombo();
+
+            lastGroutCreated.GetComponent<SpriteRenderer>().enabled = false;
+            lastGroutCreated = null;
+        }
+            isMouseDown = false;
+
+    }
+
+    void followMyMouse()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (lastGroutCreated != null)
+        lastGroutCreated.transform.position = mousePos;
+    }
+
+
+}
